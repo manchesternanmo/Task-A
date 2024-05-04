de2e4d2c59096df5922565a81b6e392a70e5c181
manchesternanmo <168119714+manchesternanmo@users.noreply.github.com>
2024-05-04 16:07:10 +0100

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    FILE *output_stream=stdout;
+    fprintf(output_stream,"Hello.My email address is:\n%s\n","wenbo.tao@student.manchester.ac.uk");
+    return 0;
+}
